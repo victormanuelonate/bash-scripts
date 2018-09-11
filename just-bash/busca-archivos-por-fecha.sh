23809379a49962463de924d19fabb279f6a7c727
victormanuelonate <victor.manuel@openmailbox.org>
2018-09-11 09:44:15 -0300

    Update busca-archivos-por-fecha.sh

--- just-bash/busca-archivos-por-fecha.sh
+++ just-bash/busca-archivos-por-fecha.sh
@@ -1,4 +1,4 @@
 mkdir /tmp/ttt
 touch -t 201808200001 /tmp/begin
 touch -t 201808202359 /tmp/end
-find /tmp/*.tmp -type f  -newer /tmp/begin -a ! -newer /tmp/end -exec cp -a {} /tmp/ttt/ \;
+find /tmp/*.tmp -type f  -newer /tmp/begin -a ! -newer /tmp/end -exec cp -a {} /tmp/ttt/ \  ;
