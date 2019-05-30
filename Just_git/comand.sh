d8583dc5e56475dadd8954fbced82519812bf3af
Victor Manuel Onate Aranguren <victor.manuel@openmailbox.org>
2019-05-30 09:54:09 -0400

    Just GIT

---
+++ Just_git/comand.sh
@@ -0,0 +1,9 @@
+ git config --global --edit
+# This is Git's per-user configuration file.
+#[user]
+# Please adapt and uncomment the following lines:
+#        name = Victor Manuel Onate Aranguren
+#        email = victor.manuel@openmailbox.org
+git add .
+git commit -m "Make SSL II "
+git push origin master
